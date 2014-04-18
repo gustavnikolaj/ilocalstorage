98c694bfd97baf742c3a6b0aa41a66ac6998f433
Gustav Nikolaj Olsen <gno@one.com>
2014-04-18 19:33:40 +0200

    add basic stuff.

---
+++ test/iLocalStorage.spec.js
@@ -0,0 +1,7 @@
+describe('iLocalStorage', function () {
+    describe('Constructor', function () {
+        it('should be a function', function () {
+            expect(iLocalStorage, 'to be a function');
+        });
+    });
+});
